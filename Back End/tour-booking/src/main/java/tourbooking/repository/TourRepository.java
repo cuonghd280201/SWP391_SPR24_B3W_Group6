230978efa4743574b32c278e82586e5c49539fe8
Duy Le <duyle210101@gmail.com>
2024-04-18 04:54:57 +0700

    add search and filter for tour

--- Back End/tour-booking/src/main/java/tourbooking/repository/TourRepository.java
+++ Back End/tour-booking/src/main/java/tourbooking/repository/TourRepository.java
@@ -1,9 +1,24 @@
 package tourbooking.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import tourbooking.entity.Tour.Tour;
 
 import java.util.UUID;
 
 public interface TourRepository extends JpaRepository<Tour, UUID> {
+    Page<Tour> findAll(Pageable pageable);
+
+    @Query("SELECT DISTINCT t FROM tour t " +
+            "JOIN t.tourDetail td " +
+            "WHERE t.title like %:keyWord% or " +
+            "t.endLocation like %:keyWord% or " +
+            "td.time like %:keyWord% or " +
+            "td.vehicle like %:keyWord% or " +
+            "td.location like %:keyWord% or " +
+            "td.food like %:keyWord% or " +
+            "td.hotel like %:keyWord%   ")
+    Page<Tour> searchByKeyWord(String keyWord, Pageable pageable);
 }
