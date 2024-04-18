b14ce5f891c6e39217ef8740503ce677f76c2279
quocthedev <qklgithub@gmail.com>
2024-04-18 16:51:17 +0700

    design better layout and add search filter

--- Front End/tour-front-end/src/pages/Staff/ListTourStaff.js
+++ Front End/tour-front-end/src/pages/Staff/ListTourStaff.js
@@ -1,42 +1,52 @@
 import React, { useState } from "react";
-import { Layout, Input, Button } from "antd";
+import { Layout, Input, Button, Select } from "antd";
 import { Row, Col } from "reactstrap";
-import { LeftOutlined, RightOutlined } from "@ant-design/icons"; // Import arrow icons
+import {
+  LeftOutlined,
+  RightOutlined,
+  PlusCircleOutlined,
+} from "@ant-design/icons"; // Import arrow icons
 
 import NavBarWebStaff from "./Navbar/NavBarWebStaff";
 import SiderBarWebStaff from "./SlideBar/SiderBarWebStaff";
 
 const { Content } = Layout;
+const { Option } = Select;
 
 const ListTourStaff = () => {
   // Dummy data
-  const dummyData = [
+  const [dummyData, setDummyData] = useState([
     {
       id: 1,
       title: "Tour 1",
-      date: "12-04-2024"
+      date: "2024-04-12",
+      price: "2.000.000",
     },
     {
       id: 2,
       title: "Tour 2",
-      date: "13-04-2024"
+      date: "2024-04-18",
+      price: "3.000.000",
     },
     {
       id: 3,
       title: "Tour 3",
-      date: "14-04-2024"
+      date: "2024-04-14",
+      price: "4.000.000",
     },
     {
       id: 4,
       title: "Tour 4",
-      date: "15-04-2024"
+      date: "2024-04-16",
+      price: "3.600.000",
     },
     {
       id: 5,
       title: "Tour 4",
-      date: "16-04-2024"
+      date: "2024-04-11",
+      price: "4.200.000",
     },
-  ];
+  ]);
 
   // State for pagination
   const [currentPage, setCurrentPage] = useState(1);
@@ -51,6 +61,36 @@ const ListTourStaff = () => {
   // Function to handle page navigation
   const goToPage = (page) => {
     setCurrentPage(page);
+  };
+
+  // Function to handle sorting by price
+  const handleSortByPrice = (value) => {
+    let sortedData = [...dummyData]; // Create a copy of the dummyData array
+
+    if (value === "asc") {
+      // Sort data by price in ascending order
+      sortedData.sort(
+        (a, b) =>
+          parseFloat(a.price.replace(/\D/g, "")) -
+          parseFloat(b.price.replace(/\D/g, ""))
+      );
+    } else if (value === "desc") {
+      // Sort data by price in descending order
+      sortedData.sort(
+        (a, b) =>
+          parseFloat(b.price.replace(/\D/g, "")) -
+          parseFloat(a.price.replace(/\D/g, ""))
+      );
+    } else if (value === "date") {
+      // Sort data by date
+      sortedData.sort((a, b) => new Date(a.date) - new Date(b.date));
+    }else if (value === "date2") {
+        // Sort data by date
+        sortedData.sort((a, b) => new Date(b.date) - new Date(a.date));
+      }
+
+    // Update the state with the sorted data
+    setDummyData(sortedData);
   };
 
   return (
@@ -69,11 +109,12 @@ const ListTourStaff = () => {
         >
           <Content>
             <div className="row">
-              <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+              <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 ">
                 <div className="page-header">
                   <h2 className="pageheader-title">Danh Sách Chuyến Đi </h2>
                 </div>
-                <div className="form-search mb-4">
+
+                {/* <div className="form-search mb-4">
                   <Row className="align-items-center">
                     <Col md="8">
                       <Input
@@ -86,17 +127,50 @@ const ListTourStaff = () => {
                       <Button color="btn-primary">Tìm kiếm</Button>
                     </Col>
                   </Row>
-                </div>
-                <div className="destination">
-                  <div className="text p-2">
-                    <p className="bottom-area d-flex">
-                      <span className="ml-auto">
-                        <a href="/createToutStaff">Tạo Chuyến Đi</a>
-                      </span>
-                    </p>
-                  </div>
-                </div>
+                </div> */}
               </div>
+            </div>
+
+            {/* Filter by price */}
+            <div className="d-flex justify-content-between mb-4">
+              <Select
+                defaultValue="Sắp xếp theo"
+                style={{ width: 250, height: 40 }}
+                onChange={handleSortByPrice}
+              >
+                <Option value="asc">Giá: Thấp đến cao</Option>
+                <Option value="desc">Giá: Cao đến thấp</Option>
+                <Option value="date">Ngày:sớm nhất đến trễ nhất</Option>
+                <Option value="date2">Ngày:trễ nhất đến sớm nhất</Option>
+              </Select>
+
+              <span style={{ display: "inline-block" }}>
+                <a
+                  href="/createToutStaff"
+                  style={{
+                    fontSize: "15px",
+                    color: "#5cb85c",
+                    textDecoration: "none",
+                    padding: "8px 16px",
+                    border: "1px solid #5cb85c",
+                    borderRadius: "4px",
+                    transition: "background-color 0.3s, color 0.3s",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                  onMouseEnter={(e) => {
+                    e.target.style.backgroundColor = "#4cae4c";
+                    e.target.style.color = "#fff";
+                  }}
+                  onMouseLeave={(e) => {
+                    e.target.style.backgroundColor = "transparent";
+                    e.target.style.color = "#5cb85c";
+                  }}
+                >
+                  <PlusCircleOutlined style={{ marginRight: "5px" }} />
+                  Tạo chuyến đi mới
+                </a>
+              </span>
             </div>
 
             {/* Display tours for the current page */}
@@ -149,11 +223,35 @@ const ListTourStaff = () => {
                                   marginBottom: 5,
                                 }}
                               >
-                                7.450.000<small>/Chuyến Đi</small>
+                                {tour.price} VNĐ
                               </p>
-                              <p className="bottom-area d-flex">
+                              <p className=" d-flex">
                                 <span className="ml-auto">
-                                  <a href="/listTourStaffDetail">
+                                  <a
+                                    href="/listTourStaffDetail"
+                                    style={{
+                                      fontSize: "15px",
+                                      color: "blueviolet",
+                                      textDecoration: "none",
+                                      padding: "8px 16px",
+                                      border: "1px solid blueviolet",
+                                      borderRadius: "4px",
+                                      transition:
+                                        "background-color 0.3s, color 0.3s",
+                                      display: "flex",
+                                      alignItems: "center",
+                                    }}
+                                    onMouseEnter={(e) => {
+                                      e.target.style.backgroundColor =
+                                        "blueviolet";
+                                      e.target.style.color = "#fff";
+                                    }}
+                                    onMouseLeave={(e) => {
+                                      e.target.style.backgroundColor =
+                                        "transparent";
+                                      e.target.style.color = "blueviolet";
+                                    }}
+                                  >
                                     Xem chi tiết
                                   </a>
                                 </span>
