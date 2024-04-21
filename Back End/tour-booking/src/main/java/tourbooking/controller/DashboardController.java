66b22353d1d38893fdab21417f879d547116d813
tranngocquocbao <166795824+tranngocquocbao@users.noreply.github.com>
2024-04-22 05:04:42 +0700

    Update DashboardController.java

--- Back End/tour-booking/src/main/java/tourbooking/controller/DashboardController.java
+++ Back End/tour-booking/src/main/java/tourbooking/controller/DashboardController.java
@@ -1,27 +1,27 @@
 package tourbooking.controller;
+import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import tourbooking.dto.UserDTO;
+import tourbooking.entity.User;
+import tourbooking.service.impl.TourServiceImpl;
 import tourbooking.service.impl.UserServiceImpl;
+
+import java.util.List;
 
 @Controller
 public class DashboardController {
 
     @Autowired
-    private UserServiceImpl userServiceIml;
+    private UserServiceImpl userService;
 
-    // Controller để trả về trang dashboard
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+        List<User> userList = userService.getAllUsers();
+        model.addAttribute("users", userList);
         return "dashboard";
-    }
-
-    // Controller để trả về số lượng người dùng dưới dạng JSON
-    @GetMapping("/api/userCount")
-    @ResponseBody
-    public Long getUserCount() {
-        return userServiceIml.getUserCount();
     }
 
 }
