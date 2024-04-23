83b00afa35d56dc42feefbfc3d7684043e2a1802
Duy Le <duyle210101@gmail.com>
2024-04-24 01:45:50 +0700

    add revenue static

---
+++ Back End/tour-booking/src/main/java/tourbooking/dto/RevenueStatisticDTO.java
@@ -0,0 +1,18 @@
+package tourbooking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RevenueStatisticDTO {
+    private LocalDate date;
+    private BigDecimal money;
+}
